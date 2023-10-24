3e5dbef6c414cdf1872959d6f7e389dacfd37db3
Mehmet <mehmetsonmez216@gmail.com>
2023-10-24 16:31:24 +0300

    setup useSelector

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,5 +1,10 @@
 export const DELETE_MOVIE = "DELETE_MOVIE";
+export const MOVIES = "MOVIES";
 
 export const deleteMovie = (id) => {
-  return ({ type: DELETE_MOVIE, payload: id });
-}
+  return { type: DELETE_MOVIE, payload: id };
+};
+
+export const movies = () => {
+  return { type: MOVIES };
+};
